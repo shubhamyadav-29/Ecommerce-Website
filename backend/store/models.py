4e5f2bf5436702a10286e22dbff1a12f3128ce4a
shubhamyadav-29 <shubhamarjun5@gmail.com>
2026-02-26 20:18:16 +0530

    Working on Serializer

--- backend/store/models.py
+++ backend/store/models.py
@@ -13,7 +13,7 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10,decimal_places=2)
-    image = models.URLField(blank=True)
+    image = models.ImageField(upload_to='products/',blank=True,null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
     
